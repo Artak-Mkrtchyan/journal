52bbca8edad4f9204e9cbb239c36648842a498ab
Artak <artak6297@gmail.com>
2019-04-22 22:02:03 +0300

    Create routes folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,35 +1,8 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { LogoutComponent } from './logout/logout.component';
-import { RegistrationComponent } from './registration/registration.component';
+import { routes } from './routes/routes';
 
-import { ArticleComponent } from './articles/article/article.component';
-import { ArticleEditComponent } from './articles/article-edit/article-edit.component';
-import { ArticleAddComponent } from './articles/article-add/article-add.component';
-
-const artileRoutes = [
-  { path: 'add', component: ArticleAddComponent },
-  {
-    path: ':id',
-    pathMath: 'prefix',
-    children: [
-      { path: '',  component: ArticleComponent },
-      { path: 'edit', component: ArticleEditComponent },
-    ]
-  },
-]
-
-const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'user/login', component: LoginComponent },
-  { path: 'user/registration', component: RegistrationComponent },
-  { path: 'user/logout', component: LogoutComponent },
-
-  { path: 'articles', children: artileRoutes },
-];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
