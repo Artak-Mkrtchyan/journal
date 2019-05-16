870c5f1b5f1aaf3b19bb948b7e67bf595a7726d1
Artak <artak6297@gmail.com>
2019-05-16 21:31:38 +0300

    Update package.json

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: npm run watch
+web: npx babel-node server.js
--- src/app/store/reducers/app.reducers.ts
+++ src/app/store/reducers/app.reducers.ts
@@ -4,8 +4,10 @@ import { IAppState } from '@store/state/app.state';
 import { playerReducers } from '@store/reducers/player.reducers';
 import { userReducers } from '@store/reducers/user.reducers';
 
-export const appReducers: ActionReducerMap<IAppState, any> = {
-  // router: routerReducer,
-  user: userReducers,
-  player: playerReducers
-};
+export function appReducers(): ActionReducerMap<IAppState, any> {
+  return {
+    // router: routerReducer,
+    user: userReducers,
+    player: playerReducers
+  };
+}
